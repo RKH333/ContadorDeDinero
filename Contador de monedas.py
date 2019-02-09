5b1cba251e38ed370e9c9c251ef4c3e7ff1d7754
RKH333 <robchenz0633@gmail.com>
2019-02-09 11:50:30 -0600

    Contador de dinero final completo con escritura de archivos

--- Contador de monedas.py
+++ Contador de monedas.py
@@ -2,6 +2,8 @@
 # Roberto Chen
 
 def contador():
+    archivo = open("Desglose.txt", "w")
+    archivo.write("Desglose de Monedas y Billetes\n")
     #Monedas
     print("-----Monedas-----")
     moneda500 = int(input("Monedas de 500: ") or 0)
@@ -18,23 +20,36 @@ def contador():
     billete1000 = int(input("Billetes de 1000: ") or 0)
 
     # Desglose de las monedas
-    print("\n-----Desglose de Monedas-----\n")
+    print("\n-----Desglose de Monedas y Billetes-----\n")
     print(str(moneda500) +  "x500: " + str(moneda500*500))
     print(str(moneda100) +  "x100: " + str(moneda100*100))
     print(str(moneda50) +  "x50: " + str(moneda50*50))
     print(str(moneda25) +  "x25: " + str(moneda25*25))
     print(str(moneda10) +  "x10: " + str(moneda10*10))
     print(str(moneda5) +  "x5: " + str(moneda5*5))
-    print("\nDesglose de Billetes")
+    print("")
     print(str(billete5000) + "x5000: " + str(billete5000*5000))
     print(str(billete2000) + "x2000: " + str(billete2000*2000))
     print(str(billete1000) + "x1000: " + str(billete1000*1000))
 
+    archivo.write("500: " + str(moneda500) + "\n")
+    archivo.write("100: " + str(moneda100) + "\n")
+    archivo.write("50: "+ str(moneda50) + "\n")
+    archivo.write("25: " + str(moneda25) + "\n")
+    archivo.write("10: " + str(moneda10) + "\n")
+    archivo.write("5: " + str(moneda5) + "\n")
+    archivo.write("5000: " + str(billete5000) + "\n")
+    archivo.write("2000: " + str(billete2000) + "\n")
+    archivo.write("1000: " + str(billete1000) + "\n")
+    
+
     total= moneda500*500+moneda100*100+moneda50*50+moneda25*25+moneda10*10+moneda5*5+billete5000*5000+billete2000*2000+billete1000*1000
     print("--------------------------------")
     print("\nTOTAL: " + str(total))
+    archivo.write("TOTAL: " + str(total))
     
     print("\nGracias por usar el sistema contador de monedas")
+    archivo.close()
 
 contador()
 input("\nPresione cualquier tecla para cerrar el programa...")
